6d809157547c3550bf75421464b543cd01625a3e
kovacspe <pet.kovacs1@gmail.com>
2022-08-04 23:44:57 +0200

    Add hints and unlock codes

--- kos/models.py
+++ kos/models.py
@@ -1,5 +1,9 @@
+
+
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import Max
+from unidecode import unidecode
 
 User = get_user_model()
 
@@ -26,26 +30,58 @@ class Puzzle(models.Model):
 
     name = models.CharField(max_length=100, default='šifra')
     game = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True)
-    solution = models.CharField(verbose_name='Riešenie', max_length=100)
+    solution = models.CharField(verbose_name='Riešenie', max_length=50)
+    unlock_code = models.CharField(
+        verbose_name='Kód na odomknutie šifry', max_length=50, null=True)
     file = models.FileField(verbose_name='Zadanie')
     pdf_solution = models.FileField(verbose_name='Riešenie v PDF')
     level = models.PositiveIntegerField(verbose_name='Úroveň/Poradie')
     location = models.TextField(null=True)
-    offline_show_delay = models.TimeField(null=True)
 
     def __str__(self):
         return self.name
 
     def team_submissions(self, team):
-        pass
+        """Vráti pokusy o odovzdanie pre daný tím"""
+        return self.submissions.filter(team=team)
 
     def has_team_passed(self, team):
         """Vráti bool či tím už šifru vyriešil"""
-        return False
+        return self.submissions.filter(team=team).aggregate(Max('correct'))
 
-    def check_solution(self, team_solution: str):
-        # TODO: Maybe use unidecode
-        return team_solution.lower() == self.solution.lower()
+    @staticmethod
+    def __check_equal(string1: str, string2: str) -> bool:
+        return unidecode(string1).lower().strip() == unidecode(string2).lower().strip()
+
+    def check_solution(self, team_solution: str) -> bool:
+        """Skontroluje riešenie"""
+        return self.__check_equal(team_solution, self.solution)
+
+    def check_unlock(self, team_submission: str) -> bool:
+        """Skontroluje odomykací kód na mieste"""
+        return self.__check_equal(team_submission, self.unlock_code)
+
+
+class Hint(models.Model):
+    """Hint"""
+    class Meta:
+        verbose_name = 'hint'
+        verbose_name_plural = 'hinty'
+
+    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='Znenie hintu')
+    show_after = models.TimeField(verbose_name='Povoliť zobrazenie po')
+    hint_penalty = models.TimeField(
+        verbose_name='Penalta za predošlé hinty', default=0)
+    count_as_penalty = models.BooleanField(
+        verbose_name='Počíta sa do penalty')
+    prerequisites = models.ManyToManyField(
+        'Hint', verbose_name='Nutné zobrať pred')
+
+    def get_time_to_take(self, team):
+        """Zostávajúci čas do hintu"""
+        # TODO: Sfunkčniť
+        return self.show_after + self.hint_penalty*team.get_penalties()
 
 
 class Category(models.Model):
@@ -80,9 +116,14 @@ class Team(models.Model):
     is_online = models.BooleanField(default=False)
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
+    hints_taken = models.ManyToManyField(Hint, verbose_name='Zobraté hinty')
 
     def __str__(self):
         return f'{self.name}'
+
+    def get_penalties(self) -> int:
+        """Spočíta počet zobratých hintov, ktoré sa rátajú ako penalty"""
+        return self.hints_taken.filter(count_as_penalty=True).count()
 
 
 class TeamMember(models.Model):
@@ -103,8 +144,9 @@ class Submission(models.Model):
     class Meta:
         verbose_name = 'odovzdanie šifry'
         verbose_name_plural = 'odovzdania šifier'
-    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+    puzzle = models.ForeignKey(
+        Puzzle, on_delete=models.CASCADE, related_name='submissions')
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     competitor_answer = models.CharField(max_length=100)
-    submited_at = models.DateTimeField()
+    submited_at = models.DateTimeField(auto_now=True, auto_created=True)
     correct = models.BooleanField()  # Neviem ci bude nutné nechám na zváženie
