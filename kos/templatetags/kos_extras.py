c197f5330bd7dd58dee4b0add08d555bd9dc6340
Michal Masrna <m.masrna1@gmail.com>
2022-09-04 16:16:42 +0200

    added custom tags for more hint methods

--- kos/templatetags/kos_extras.py
+++ kos/templatetags/kos_extras.py
@@ -6,3 +6,13 @@ register = template.Library()
 @register.simple_tag
 def get_hint_available_time(hint, team):
     return hint.time_when_will_be_unlocked(team)
+
+
+@register.simple_tag
+def all_prerequisites_met(hint, team):
+    return hint.all_prerequisites_met(team)
+
+
+@register.simple_tag
+def can_team_take_hint(hint, team):
+    return hint.can_team_take(team)
