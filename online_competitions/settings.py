644812294df259a1edbb971fb2c7f7d2cb5ce7e0
kovacspe <pet.kovacs1@gmail.com>
2022-09-07 23:12:57 +0200

    Production settings

--- online_competitions/settings.py
+++ online_competitions/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^(txxj1p(@hfc)@u7u#d1h$i##m+g*=m!wzu^%zs3r+lw4mlh_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['kos.strom.sk']
 
 
 # Application definition
