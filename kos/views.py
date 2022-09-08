0dbfdef58b6de4f6542586bb22d0a332979aa4cb
kovacspe <pet.kovacs1@gmail.com>
2022-09-08 14:26:58 +0200

    Handling no permission on hint

--- kos/views.py
+++ kos/views.py
@@ -201,7 +201,6 @@ class GameView(GetTeamMixin, DetailView):
         if not puzzle.can_team_submit(team):
             messages.error(request, 'Odpoveď nie je možné odovzdať')
             return redirect('kos:game')
-            # return HttpResponseForbidden()
         if not puzzle.can_team_see(team):
             is_correct = puzzle.check_unlock(answer)
             Submission.objects.create(
@@ -268,6 +267,10 @@ class HintView(GetTeamMixin, UserPassesTestMixin,  DetailView):
     def post(self, request, *args, **kwargs):
         """Pridaj hint"""
         self.get_team().hints_taken.add(self.get_object())
+        return redirect('kos:game')
+
+    def handle_no_permission(self):
+        messages.error(self.request, 'Hint nie je možné zobrať')
         return redirect('kos:game')
 
 
