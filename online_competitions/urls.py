d238747cf81464470f081a94ad72ad50e457a24a
kovacspe <pet.kovacs1@gmail.com>
2022-09-08 17:01:30 +0200

    homepage redirect

--- online_competitions/urls.py
+++ online_competitions/urls.py
@@ -1,9 +1,11 @@
 
 
 from django.contrib import admin
+from django.contrib.flatpages.views import flatpage
 from django.urls import include, path
 
 urlpatterns = [
+    path('/', flatpage, {'url': '/pravidla/'}, name='home'),
     path('admin/', admin.site.urls),
     path('kos/', include('kos.urls', namespace='kos')),
     path('mas-problem/', include('mas_problem.urls', namespace='mas-problem'))
