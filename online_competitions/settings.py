c3c6da2127d46264281b78a31d06a47ad44c53a3
kovacspe <pet.kovacs1@gmail.com>
2022-11-23 22:12:13 +0100

    Turn on debug

--- online_competitions/settings.py
+++ online_competitions/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^(txxj1p(@hfc)@u7u#d1h$i##m+g*=m!wzu^%zs3r+lw4mlh_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kos.strom.sk', 'localhost']
 
