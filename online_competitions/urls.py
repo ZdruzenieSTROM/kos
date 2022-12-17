3f264b221a784dff710a3357ba1c0ab897e7c222
kovacspe <pet.kovacs1@gmail.com>
2022-12-17 10:34:41 +0100

    fix URL paths

--- online_competitions/urls.py
+++ online_competitions/urls.py
@@ -5,8 +5,7 @@ from django.contrib.flatpages.views import flatpage
 from django.urls import include, path
 
 urlpatterns = [
-    path('/', flatpage, {'url': '/pravidla/'}, name='home'),
     path('admin/', admin.site.urls),
-    path('kos/', include('kos.urls', namespace='kos')),
+    path('', include('kos.urls', namespace='kos')),
 ]
 handler404 = 'kos.views.view_404'
