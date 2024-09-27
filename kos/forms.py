913a0d65aa8d253c1b7335992cc9b92fd7ba9f09
Michal Masrna <m.masrna1@gmail.com>
2024-09-27 17:08:42 +0200

    added team_name field to team edit form

--- kos/forms.py
+++ kos/forms.py
@@ -130,6 +130,9 @@ class CustomResetPasswordFromKey(ResetPasswordKeyForm):
 
 class EditTeamForm(forms.Form):
     """Form na úpravu tímových údajov"""
+    team_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control main-input'}),
+        label='Názov tímu')
     team_member_1 = forms.CharField(
         widget=forms.TextInput(attrs={'class': 'form-control main-input'}),
         label='1. člen tímu')
